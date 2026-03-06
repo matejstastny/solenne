6e06969dd9c1423e307bcdb052fc5c5b1e02d178
Matej Stastny <matysta@outlook.com>
2026-03-06 12:59:42 -0800

    Tested assets in code

--- core/src/main/java/solene/App.java
+++ core/src/main/java/solene/App.java
@@ -5,28 +5,48 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+// Main app class. Does not include main method
 public class App extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+
+    // Textures
+    private Texture logo;
+    private Texture player;
+    private Texture tree;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        logo = new Texture("libgdx.png");
+        player = new Texture("player.png");
+        tree = new Texture("tree.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        input();
+        logic();
+        draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        logo.dispose();
+    }
+
+    public void input() {
+    }
+
+    public void logic() {
+    }
+
+    public void draw() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(logo, 140, 210);
+        batch.draw(player, 150, 220);
+        batch.draw(tree, 160, 230);
+        batch.end();
     }
 }
